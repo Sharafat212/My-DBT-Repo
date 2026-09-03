69486fd224bbbacf87d11fb0ae867c4f9353f649
Sharafat212 <sharafatboss9@gmail.com>
2026-09-03 15:04:31 +0500

    Fix macro name in gold model

--- models/gold/gold_summary_dpt.sql
+++ models/gold/gold_summary_dpt.sql
@@ -1,4 +1,4 @@
 SELECT 
-    {{ handle_null_numeric('COUNT(*)') }} AS total_records,
-    SUM({{ handle_null_numeric('amount') }}) AS total_amount
+    {{ handle_null_amount('COUNT(*)') }} AS total_records,
+    SUM({{ handle_null_amount('amount') }}) AS total_amount
 FROM {{ ref('silver_cleaned_dpt') }}
