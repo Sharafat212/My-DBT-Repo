8a31d125d62bf42f8643130dae7f89d65cd506a5
Sharafat212 <sharafatboss9@gmail.com>
2026-09-04 16:39:18 +0500

    Fix gold_employees casing and add gold_department_summary model

--- models/gold/gold_employees.sql
+++ models/gold/gold_employees.sql
@@ -1,18 +1,14 @@
-{{ config(
-    materialized='table'
-) }}
-
 SELECT 
     Department,
-
-    -- Employees count (Silver layer ka Emp_id)
-    COUNT(Emp_id) AS Total_Employees,
-
-    -- Total Salary (Silver layer ka Salary column)
-    {{ calculate_total('Salary') }} AS Total_Salary,
-
+    
+    -- Employees count
+    COUNT(id) AS Total_Employees,
+    
+    -- Total Salary
+    {{ calculate_total('salary') }} AS Total_Salary,
+    
     -- Average salary
-    {{ calculate_average('Salary', 2) }} AS Avg_Salary
+    {{ calculate_average('salary', 2) }} AS Avg_Salary
 
 FROM {{ ref('silver_employees') }}
 
