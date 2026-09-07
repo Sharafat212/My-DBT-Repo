aa44a58b69bfe13d40d8ddc6a508e49180ff435b
Sharafat212 <sharafatboss9@gmail.com>
2026-09-07 16:01:45 +0500

    Fix gold model config and add deleted_count

--- models/gold/gold_students.sql
+++ models/gold/gold_students.sql
@@ -1,10 +1,16 @@
+{{ config(
+    materialized='table',
+    schema='gold_db'
+) }}
+
 select
     class,
     subject,
     count(student_id)   as total_students,
     round(avg(marks), 2) as avg_marks,
     max(marks)           as max_marks,
-    min(marks)           as min_marks
+    min(marks)           as min_marks,
+    count(case when is_deleted = true then 1 end) as deleted_count
 from {{ ref('silver_students') }}
 where is_deleted = false
 group by class, subject
